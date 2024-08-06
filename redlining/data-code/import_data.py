f84c8446efde90a9e043ec1d02caebe8eacb8ab5
Michaela <michaela.philip@emory.edu>
2024-08-05 22:22:55 -0400

    additional comments

--- redlining/data-code/import_data.py
+++ redlining/data-code/import_data.py
@@ -1,12 +1,10 @@
 import pandas as pd
+print('pandas in')
 import numpy as np
-import sys
-import os
-
-# script_dir = os.path.dirname(os.path.realpath(__file__))
-# sys.path.append(os.path.join(script_dir, '..'))
+print('numpy in')
 
 import gzip
+print('gzip in')
 
 colspecs = [(0, 4), (4, 10), (10, 18), (18, 28), (28, 30), (30, 32), (32, 36), (36, 37), (37, 38), (38, 40), (40, 49), (49, 53), (53, 63), (63, 64), (64, 67), (67, 71), (71, 77), (77, 79), (79, 115)]
 columns = ['year', 'sample', 'serial', 'hhwt', 'stateicp', 'statefip', 'countyicp', 'gq', 'ownershp', 'ownershpd', 'enumdist', 'pernum', 'perwt', 'race', 'raced', 'occ', 'incwage', 'versionhist', 'histid']

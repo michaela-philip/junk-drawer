3756116f0eb8a62d6ed2cedc29da17133467b475
michaela-philip <122124294+michaela-philip@users.noreply.github.com>
2024-08-12 14:20:53 -0400

    import census data as regular fwf not through gzip

--- redlining/data-code/import_data_pc.py
+++ redlining/data-code/import_data_pc.py
@@ -9,9 +9,14 @@ print('gzip in')
 colspecs = [(0, 4), (4, 10), (10, 18), (18, 28), (28, 30), (30, 32), (32, 36), (36, 37), (37, 38), (38, 40), (40, 49), (49, 53), (53, 63), (63, 64), (64, 67), (67, 71), (71, 77), (77, 79), (79, 115)]
 columns = ['year', 'sample', 'serial', 'hhwt', 'stateicp', 'statefip', 'countyicp', 'gq', 'ownershp', 'ownershpd', 'enumdist', 'pernum', 'perwt', 'race', 'raced', 'occ', 'incwage', 'versionhist', 'histid']
 
-with gzip.open('data/input/usa_00004.dat', 'rb') as f:
-    df = pd.read_fwf(f, colspecs = colspecs, header = None)
-    print('census data loaded')
+# with gzip.open('data/input/usa_00004.dat', 'rb') as f:
+#     df = pd.read_fwf(f, colspecs = colspecs, header = None)
+#     print('census data loaded')
+    
+data = 'data/input/usa_00004.dat'
+df = pd.read_fwf(data, colspecs = colspecs, header = None)
+print('census data loaded')
+
 df.columns = columns
 df['ownershp'] = np.where(df['ownershp'] == 1, 1, 0)
 

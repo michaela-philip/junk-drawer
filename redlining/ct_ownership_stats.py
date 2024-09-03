af924a6ab40c8e678b31c559b1ef8ad4e42cd48e
Michaela <michaela.philip@emory.edu>
2024-09-03 10:10:51 -0400

    histogram and pivot table

--- redlining/ct_ownership_stats.py
+++ redlining/ct_ownership_stats.py
@@ -1,24 +1,35 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas.plotting import table
 
-from redlining.census_tract_classification import tract as tract
+from census_tract_classification import tract as tract
 
-tract_by_grade_10 = tract.groupby('grade_10')['ownershp'].agg(['min', 'max', 'mean', 'median']).reset_index()
-# tract_by_grade_4 = tract.groupby('grade_4')['ownershp'].agg(['min', 'max', 'mean', 'median']).reset_index()
-
-def get_histograms(column):
+def get_histograms(column, categorynum):
     unique_grades = sorted(tract[column].unique())
-    fig, axes = plt.subplots(2, 5, figsize = (18, 10), sharey = True, sharex = True)
+    fig, axes = plt.subplots(2, int(len(unique_grades) / 2), figsize = (18, 10), sharey = True, sharex = True)
     axes = axes.flatten()
     for ax, grade in zip(axes, unique_grades):
         tract[tract[column] == grade]['ownershp'].hist(ax=ax)
         ax.set_title(f'Grade {grade}')
     plt.subplots_adjust(hspace=0.4)
-    fig.suptitle('Home Ownership Rates by HOLC Grade')
+    fig.suptitle(f'Home Ownership Rates by HOLC Grade ({categorynum} Categories)')
     fig.supylabel('Tracts')
     fig.savefig(f'redlining/data/output/ownership_hist_{column}.jpeg', dpi = 300)
 
-get_histograms('grade_10')
-get_histograms('grade_4')
+get_histograms('grade_10', 10)
+get_histograms('grade_4', 4)
 
+def get_stats_table(column, categorynum):
+    pivot_table = tract.pivot_table('ownershp', index=column, aggfunc=['min', 'max', 'mean', 'median'])
+    pivot_table.columns = ['Min', 'Max', 'Mean', 'Median']
+    fig, ax = plt.subplots(figsize=(10, 4))
+    ax.axis('off')
+    tbl = table(ax, np.round(pivot_table, 3), loc='center', cellLoc='center', colWidths=[0.1]*len(pivot_table.columns))
+    plt.title(f'Home Ownership Summary Statistics by HOLC Grade ({categorynum} Categories)')
+    plt.tight_layout()
+    plt.savefig(f'redlining/data/output/pivot_table_{column}.jpeg', dpi=300, bbox_inches = 'tight')
+
+
+get_stats_table('grade_10', 10)
+get_stats_table('grade_4', 4)

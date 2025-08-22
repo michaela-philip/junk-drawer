338907bf930ef9557df888c9925c88a8d2002ad9
michaela-philip <michaela.philip@emory.edu>
2025-08-22 14:31:20 -0400

    update filepath

--- kidneys/data-code/import_census.py
+++ kidneys/data-code/import_census.py
@@ -1,13 +1,10 @@
 import pandas as pd
-import geopandas as gpd
 import gzip
 
-colspecs = [(0, 4), (4, 10), (10, 18), (18, 31), (31, 41), (41, 54), (54, 56), (56, 60), (60, 72), (72, 73), (73, 77), 
-            (77, 87), (87, 88), (88, 91), (91, 92), (92, 94), (94, 95), (95, 102), (102, 109), (109, 112)]
-columns = ['year', 'sample', 'serial', 'cbserial', 'hhwt', 'cluster', 'stateicp', 'countyicp', 'strata', 'gq', 
+colspecs = [(0, 4), (4, 8), (8, 14), (14, 22), (22, 35), (35, 45), (45, 58), (58, 60), (60, 63), (63, 75), (75, 76), 
+            (76, 80), (80, 90), (90, 91), (91, 94), (94, 95), (95, 97), (97, 98), (98, 105), (105, 112), (112, 115)]
+columns = ['year', 'multyear', 'sample', 'serial', 'cbserial', 'hhwt', 'cluster', 'statefip', 'countyfip', 'strata', 'gq', 
            'pernum', 'perwt', 'race', 'raced', 'empstat', 'empstatd', 'labforce', 'inctot', 'ftotinc', 'poverty']
 
-census = pd.read_fwf(gzip.open('data/input/usa_00006.dat.gz'), colspecs = colspecs, header = None, columns = columns)
+census = pd.read_fwf(gzip.open('data/input/usa_00008.dat.gz'), colspecs = colspecs, header = None, names = columns)
 census.to_pickle('data/input/census.pkl')
-
-icpsr_crosswalk = pd.read_csv('data/input/icpsrcnt.csv', dtype = str)
